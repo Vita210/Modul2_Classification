c5146b9dce40f2803b4ca63431116fd33ea51db3
Vita210 <220711706@students.uajy.ac.id>
2025-04-09 14:36:03 +0700

    Update data_loader.py

--- singlelabel/utils/data_loader.py
+++ singlelabel/utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset
     """
-    df = pd.read_csv('D:\\Kuliah\\SEM 6\\BahasaAlami\\Modul 2\\modul2_Classification_Text\\data\\train_preprocess.csv')
+    df = pd.read_csv('data\train_preprocess.csv')
     return df
